e5cb5ecb51b68fe2edd29aa5224a60f29b79971f
r3dhyka <ggredhyka@gmail.com>
2026-03-12 13:38:47 +0700

    Update plot_data.py

--- Qudev/codes/Python/T1_Measurements/plot_data.py
+++ Qudev/codes/Python/T1_Measurements/plot_data.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load CSV (skip header row)
-data = np.loadtxt("D:/trina/TA_internal/waveform_0.csv", delimiter=",", skiprows=1)
+number = int(input("enter the waveform number: "))
+data = np.loadtxt(f"D:/trina/TA_internal/waveform_{number}.csv", delimiter=",", skiprows=1)
 
 time = data[:, 0]
 voltage = data[:, 1]
@@ -15,3 +16,4 @@ plt.ylabel("Voltage (V)")
 plt.title("Oscilloscope Waveform")
 plt.grid(True)
 plt.show()
+
